80f42a14e5a2dff9135dacff264e2a20a1dee513
Rabbi Hasan Riaz <147678292+rabby-riaz@users.noreply.github.com>
2024-10-05 23:07:25 +0600

    Add files via upload

---
+++ sub,sub,dev of two numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 54,y =6;
+    int sum = x +y;
+    int sub = x -y;
+    int dev = x/y;
+    int mul = x*y;
+
+
+
+
+    cout << "the result = " << sum<< endl;
+    cout <<  "the result = " <<sub << endl;
+    cout <<  "the result = " <<dev<< endl;
+    cout <<  "the result = " <<mul;
+    return 0;
+}
